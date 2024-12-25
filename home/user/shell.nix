6373014574d31a21aca89a4bc777805070dabcbe
DACHXY <Danny10132024@gmail.com>
2024-12-25 17:15:51 +0800

    add fish shell alias

--- home/user/shell.nix
+++ home/user/shell.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  shellAlias = {
+    ls = "exa";
+    cat = "bat";
+    rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
+    fullClean = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+    setWindows = "sudo bootctl set-oneshot auto-windows";
+    goWin = "sudo bootctl set-oneshot auto-windows && reboot";
+    goBios = "sudo bootctl set-onshot auto-reboot-to-firmware-setup && reboot";
+  };
+in
+{
   programs = {
     # nushell = {
     #   enable = true;
     #   configFile.source = ../config/nushell/config.nu;
     #   envFile.source = ../config/nushell/env.nu;
     # };
+
 
     fish = {
       enable = true;
