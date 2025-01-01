4b6451294bf3c30e13f6dada2c0caaaa09011d45
DACHXY <Danny10132024@gmail.com>
2025-01-01 16:28:52 +0800

    fxi: waybar restart script

--- home/config/scripts/waybarRestart.sh
+++ home/config/scripts/waybarRestart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Restart Waybar and swaync
-
+XDG_CONFIG_HOME="$HOME/.dummy" # Prevent swaync use default gtk theme
 killall .waybar-wrapped
 killall .swaync-wrapped
 waybar -c ~/.config/waybar/config.json -s ~/.config/waybar/style.css &
--- system/modules/virtualization.nix
+++ system/modules/virtualization.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   programs.virt-manager.enable = true;
@@ -15,11 +15,24 @@
 
     libvirtd = {
       enable = true;
-      qemu.swtpm.enable = true;
-      qemu.ovmf.enable = true;
-      qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
     };
 
     spiceUSBRedirection.enable = true;
+  };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    };
   };
 }
