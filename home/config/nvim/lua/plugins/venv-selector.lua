fe1d9adfa8ae42f58a5870974cc5e15b3d359b62
DACHXY <Danny10132024@gmail.com>
2025-01-03 01:43:27 +0800

    update: nvim venv-selector

--- home/config/nvim/lua/plugins/venv-selector.lua
+++ home/config/nvim/lua/plugins/venv-selector.lua
@@ -5,12 +5,19 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap" },
-    opts = {
-      name = { "venv", ".venv" },
-      parents = 2,
-      auto_refresh = true,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    event = "VeryLazy",
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
+    end,
+    keys = {
+      { "<leader>cv", "<cmd>VenvSelect<cr>" },
+    },
   },
 }
