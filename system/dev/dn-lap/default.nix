fb06f7eee01da7a38965230fad68494cc2c00741
DACHXY <Danny10132024@gmail.com>
2025-07-14 16:22:22 +0800

    chore: add shortcut ssh key

--- system/dev/dn-lap/default.nix
+++ system/dev/dn-lap/default.nix
@@ -30,5 +30,6 @@
   users.users."${settings.personal.username}".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSHkPa6vmr5WBPXAazY16+Ph1Mqv9E24uLIf32oC2oH danny@phone.dn"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSAOufpee7f8D8ONIIGU3qsN+8+DGO7BfZnEOTYqtQ5 danny@pre7780.dn"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMj/LeB3i/vca3YwGNpAjf922FgiY2svro48fUSQAjOv Shortcuts on :D"
   ];
 }
