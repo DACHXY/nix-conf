62c8d4f2ac45ca83c68c601baad71566c51999ba
DACHXY <dachxy@dnywe.com>
2026-05-23 01:06:39 +0800

    feat: add personal github access token and nix-ld

--- modules/users/danny/nixos.nix
+++ modules/users/danny/nixos.nix
@@ -1,12 +1,6 @@
 { config, ... }:
 {
-  flake.modules.nixos.danny =
-    { ... }@nixosArgs:
-    {
-      imports = [ config.flake.modules.generic.danny ];
-
-      home-manager.users.${nixosArgs.config.my.user.name} = {
-        imports = [ config.flake.modules.homeManager.danny ];
-      };
-    };
+  flake.modules.nixos.danny = {
+    imports = [ config.flake.modules.generic.danny ];
+  };
 }
