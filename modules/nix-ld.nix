f81e7ba098e8234d22994c20a95aa971f8542cd6
DACHXY <dachxy@dnywe.com>
2026-05-23 01:14:36 +0800

    update: add more libraries to nix-ld

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -19,6 +19,34 @@
           util-linux
           xz
           systemd
+
+          glib
+
+          # Inspired by steam
+          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/st/steam/package.nix#L36-L85
+          networkmanager
+          vulkan-loader
+          libgbm
+          libdrm
+          libxcrypt
+          coreutils
+          pciutils
+          zenity
+
+          # Verified games requirements
+          libxt
+          libxmu
+          libogg
+          libvorbis
+          SDL
+          SDL2_image
+          glew_1_10
+          libidn
+          tbb
+
+          # Appimages need fuse, e.g. https://musescore.org/fr/download/musescore-x86_64.AppImage
+          fuse
+          e2fsprogs
         ];
       };
     };
