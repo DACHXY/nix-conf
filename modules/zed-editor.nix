5f91d5e64d0906e2063ce4d5fc131aaefed95f34
danny <dachxy@dnywe.com>
2026-05-23 20:37:07 +0800

    Update zed-editor.nix

--- modules/zed-editor.nix
+++ modules/zed-editor.nix
@@ -1,4 +1,19 @@
 {
+  flake.modules.nixos.base = {pkgs, ...}: {
+    programs.nix-ld = {
+      libraries = with pkgs;[
+        openssl
+        zlib
+      ];
+    };
+  };
+
+  flake.modules.darwin.gui = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+     nixd
+    ];
+  };
+
   flake.modules.homeManager.zed =
     { lib, pkgs, ... }:
     {
@@ -6,6 +21,7 @@
         enable = true;
         enableMcpIntegration = false;
         installRemoteServer = true;
+        extraPackages = with pkgs; [ nixd nixfmt ];
 
         extensions = [
           "nix"
@@ -24,9 +40,39 @@
 
         userKeymaps = [
           {
-            "context" = "Workspace";
-            "bindings" = {
+            context = "(vim_mode == helix_normal || vim_mode == helix_select) && !menu";
+            bindings = {
               "space space" = "file_finder::Toggle";
+            };
+            unbind = {
+              "space f" = "file_finder::Toggle";
+            };
+          }
+          {
+            context = "Editor && VimControl && !VimWaiting && !menu";
+            bindings = {
+                    "-"= "project_panel::ToggleFocus";
+                    "ctrl-h"= "workspace::ActivatePaneLeft";
+                    "ctrl-j"= "workspace::ActivatePaneDown";
+                    "ctrl-k"= "workspace::ActivatePaneUp";
+                    "ctrl-l"= "workspace::ActivatePaneRight";
+                    "space n"= "workspace::ToggleLeftDock";
+            };
+          }
+          {
+            context = "Workspace";
+            bindings = {
+              "ctrl-/" = "terminal_panel::Toggle";
+            };
+            unbind = {
+              "ctrl-`" = "terminal_panel::Toggle";
+            };
+          }
+          {
+            context = "Workspace";
+            bindings = {
+              "ctrl-s" = "workspace::Save";
+              "cmd-s" = "workspace::Save";
             };
           }
         ];
@@ -34,13 +80,31 @@
           auto_update = false;
           vim_mode = true;
           base_keymap = "VSCode";
-          hour_format = "hour24";
           # Load flake.nix
-          load_direnv = "shell_hook";
+          # load_direnv = "shell_hook";
+
+          languages = {
+            "Nix" = {
+              formatter.external = {
+                command= "nixfmt";
+                arguments = ["--quiet" "--"];
+              };
+              language_servers = [ "nixd" "!nil" ];
+            };
+          };
 
           node = {
             path = lib.getExe pkgs.nodejs;
             npm_path = lib.getExe' pkgs.nodejs "npm";
+          };
+
+          lsp = {
+            nixd.settings.diagnostic.supress = [ "sema-extra-with" ];
+            nix = {
+              binary = {
+                path = lib.getExe pkgs.nixd;
+              };
+            };
           };
 
           terminal = {
@@ -66,13 +130,7 @@
             option_as_meta = false;
             button = false;
             shell = "system";
-            toolbar.title = true;
             working_directory = "current_project_directory";
-          };
-
-          lsp = {
-            rust-analyzer.binary.path_lookup = true;
-            nix.binary.path_lookup = true;
           };
         };
       };
